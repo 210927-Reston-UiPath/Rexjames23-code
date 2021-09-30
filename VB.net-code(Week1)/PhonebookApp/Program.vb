edfcf724153271aed343f58afe4e5280f2904a28
Colton Reed <you@example.com>
2021-09-30 13:57:52 -0500

    added phone book app no gitignore yet

---
+++ VB.net-code(Week1)/PhonebookApp/Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Howdy, what is your name?")
+       Dim result As String
+       result = Console.ReadLine()
+       Console.WriteLine("hello " & result)
+       Console.WriteLine("how's it hangin?")
+    End Sub
+End Module
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 I made this in the cmd line.
 1+1=2 yo
 I just added this line
+added my projects
